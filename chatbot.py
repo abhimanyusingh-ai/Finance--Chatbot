c180a1b7a9dedcaac8d525ab5192ea4a36c40cf7
Abhimanyu Pal Singh <abhimanyuplsngh@gmail.com>
2026-03-17 05:54:22 +0000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,3 @@
-# Finance-Chatbot
-Finance Assistant Chatbot  A Python-based chatbot that answers basic finance and trading queries (forex, crypto, risk management). Uses rule-based NLP with intent matching and JSON data. Demonstrates conversational AI fundamentals and modular design for easy future enhancements.
 import json
 import random
 
